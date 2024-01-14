0568c012f27200b65054c65356bc5f2fe18b2978
vininirim <steam.faceit.00@mail.ru>
2024-01-14 21:03:47 +0300

    второе практическое задание

--- lesson_1/main.py
+++ lesson_1/main.py
@@ -11,3 +11,14 @@ def average(numbers: list | tuple) -> int | float:
 
 
 print(average([1, 2, 3, 4, 5]))
+
+
+# Проверить поле на то, чтобы в нем было только одно слово
+
+def split_words(text: str) -> list:
+    words: list[str] = text.split()
+    return words
+
+
+words = split_words('ПРивет Арсений и Даниил сегодня занятие')
+print(words, len(words))
