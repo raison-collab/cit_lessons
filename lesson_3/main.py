3036fdfefd197b52814c4e253145e1d58230ea97
vininirim <steam.faceit.00@mail.ru>
2024-01-21 20:17:51 +0300

    практическое задание 2

--- lesson_3/main.py
+++ lesson_3/main.py
@@ -1,15 +1,23 @@
-def reverse_string(text: str) -> str:
-    t = reversed(text)
-    return ''.join(t)
+def even_numbers(numbers: list[int]) -> list[int]:
+    l = []
+
+    for num in numbers:
+        if num % 2 == 0:
+            l.append(num)
+
+    return l
 
 
 def main():
-    f1 = 'hello'
-    f2 = 'world'
+    f1 = [1, 2, 3, 4, 54]
+    f2 = [3, 5, 1]
+    f3 = []
 
-    print(reverse_string(f1))
-    print(reverse_string(f2))
-    print(reverse_string(f1+f2))
+    print(even_numbers(f1))
+    print(even_numbers(f2))
+    print(even_numbers(f3))
+    print(even_numbers(f1+f2))
+
 
 
 main()
