af2b854390186989fa20376b2fe40941743b1580
vininirim <steam.faceit.00@mail.ru>
2024-01-21 20:40:04 +0300

    практическое задание 4

--- lesson_3/main.py
+++ lesson_3/main.py
@@ -1,21 +1,21 @@
-def min_max(numbers: list) -> list:
-    if not numbers:
-        return []
+def in_range(numbers: list, start: int, end: int) -> list:
+    interval = [num for num in range(start, end+1)]
 
-    l = [min(numbers), max(numbers)]
+    l = []
+
+    for num in numbers:
+        if num in interval:
+            l.append(num)
 
     return l
 
 
 def main():
-    f1 = [1, 465, 56, 123445]
-    f2 = [-1231231, 123123, 0, 213123, 34, -40394]
-    f3 = []
+    f1 = in_range(list(range(100)), 96, 105)
+    f2 = in_range([4, 7, 9, 124, 12], 5, 15)
 
-    print(min_max(f1))
-    print(min_max(f2))
-    print(min_max(f3))
-    print(min_max(f1+f2))
+    print(f1)
+    print(f2)
 
 
 main()
